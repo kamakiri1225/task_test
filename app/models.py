e330011b027c21037031034a29bf8edf9c689fa9
kamakiri1225 <kusumuratakuya@yahoo.co.jp>
2021-01-19 14:13:17 +0900

    2d commit

--- app/models.py
+++ app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    category = models.CharField(max_length=100)
+    pub_date = models.DateTimeField('add publish')
+
+    def __str__(self):
+        return self.category
+
+class Choise(models.Model):
+    question = models.ForeignKey(Category, on_delete=models.CASCADE)
+    question_num = models.IntegerField(default=0)
+    choise_text1 = models.CharField(max_length=200,null=True)
+    choise_text2 = models.CharField(max_length=200,null=True)
+    choise_text3 = models.CharField(max_length=200,null=True)
+    choise_text4 = models.CharField(max_length=200,null=True)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f'問題{str(self.question_num)}'
--- app/views.py
+++ app/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from .models import Category, Choise
+
 
 # Create your views here.
+def index(request):
+    category_list = Category.objects.order_by('-pub_date')[:5]
+    context = {
+        'category_list':category_list,
+    }
+    return render(request, 'app/index.html', context)
+
+def detail(request, question_id):
+    print(Choise)
+    question = get_object_or_404(Choise, pk=question_id)
+    return render(request, 'app/detail.html', {'question': question})
